7f9ca693dd392e9db9177f6370caeb0c396c9aa6
Veronika Havlátová <veronika.havlatova@quanti.cz>
2020-11-28 22:52:03 +0100

    change

--- src/app/+barrel/barrel.component.ts
+++ src/app/+barrel/barrel.component.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/core';
 /**
  * We're loading this component asynchronously
- * We are using some magic with es6-promise-loader that will wrap the module with a Promise
+ * We are using asome magic with es6-promise-loader that will wrap the module with a Promise
  * see https://github.com/gdi2290/es6-promise-loader for more info
  */
 
